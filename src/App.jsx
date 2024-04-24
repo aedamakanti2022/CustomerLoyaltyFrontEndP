6cd66f4bde1c8d9461d848371f4a3809d60bb058
aedamakanti2022 <159940264+aedamakanti2022@users.noreply.github.com>
2024-04-24 13:44:01 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  BusinessDashboard,
+  BusinessLogin,
+  Dashboard,
+  HomeLayout,
+  Landing,
+  Login,
+  Logout,
+  Register,
+  RegisterBusiness,
+} from "./pages";
+import { ToastContainer } from "react-toastify";
+import ForgetPassword from "./pages/ForgetPassword";
+import ForgetPasswordBusiness from "./pages/ForgetPasswordBusiness";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "customerLogin",
+        element: <Login />,
+      },
+      {
+        path: "businessLogin",
+        element: <BusinessLogin />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "forgetPassword",
+        element: <ForgetPassword />,
+      },
+      {
+        path: "forgetPasswordBusiness",
+        element: <ForgetPasswordBusiness />,
+      },
+
+      {
+        path: "businessRegister",
+        element: <RegisterBusiness />,
+      },
+      {
+        path: "customerDashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "businessDashboard",
+        element: <BusinessDashboard />,
+      },
+      {
+        path: "logout",
+        element: <Logout />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+      <ToastContainer position="top-center" />
+    </>
+  );
+}
+
+export default App;
